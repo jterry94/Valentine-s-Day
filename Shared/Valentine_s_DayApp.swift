c6bc99e3261c1de79735f096d0754600d3fb38de
jterry94 <terryj@iit.edu>
2022-01-23 14:52:07 -0600

    Initial Commit

---
+++ Shared/Valentine_s_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valentine_s_DayApp.swift
+//  Shared
+//
+//  Created by Jeff_Terry on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Valentine_s_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
